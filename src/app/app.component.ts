554dc5d101cb6b711feb99d5a5c974bd5dae7c03
Jared <u202020239@upc.edu.pe>
2022-10-04 16:11:23 -0500

    Add Options Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UrbanSolution-FreePlace';
+  //Add options routers
+  options=[
+    {path: '/parking-lots', title: 'Search Parking'},
+    {path: '/parking-graphs', title: 'Parking Graphs'},
+    {path: '/profile', title: 'Profile'}
+  ]
+
 }
